e89d51f93e597bab6b2585d02fc4b76af0f8f067
Damien Maure <damien.maure@ynov.com>
2018-01-09 17:05:06 +0100

    commit de prévoyance

--- app.js
+++ app.js
@@ -8,12 +8,12 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 // Route pour afficher tous les patients
 app.get('/patients', (req, res) => {
-  // res.sendFile(path.join(`${__dirname}/test.json`));
+  res.sendFile(path.join(`${__dirname}/test.json`));
 });
 
 // Route pour afficher un patient spécifique
 app.get('/patient/:id', (req, res) => {
-
+	
 });
 
 // Route pour ajouter un patient
